cbdd1a21cf92dccf50ae18de1111c66ba5bd8247
David O'Dwyer <public.dpod@gmail.com>
2021-01-12 12:29:30 +0000

    edit cloned template with personal information; delete unnecessary files

--- data.js
+++ data.js
@@ -1,48 +1,32 @@
 const main = {
-  name: 'Your Name',
-  mail: 'your.mail@gmail.com',
-  img: 'https://organicthemes.com/demo/profile/files/2018/05/profile-pic.jpg',
-  role: ['Full Stack Developer'],
+  name: 'David O\'Dwyer',
+  mail: 'davidpodwyer@gmail.com',
+  img: 'https://previews.dropbox.com/p/thumb/ABAJQZWzD3vvi9C7s7AwcU0LRnsLLbbbD_fSHbEdkRCpfpTJ1oR3dQOe2rah6_EVw1CDYg5EOGqoPmxW2Y05xBV04U-gmDzjaIkROVkzrLR_D3bQw0s1ZLHpzMRxaVxNVd8XLauTAMuJCXqCQ5V5E1zTWlVgMLhi7ILh9UdBi6TySxPE2Ig8XwlgUwOvS00GmY_UukGFDxXg7DNm4NPVQ8eAazL3xS1gPN-dja5ZH9zy9X-oUZGOuX2w_bnaHCCKxX400uM66hk886ihxH3c5_KJlVKYPviO6Ezqi4V2T6rk7oMsGghJdEisRz46mY4hBfHk03XCdlSVifVAePA5KOpC31OvX5RNfFZka6zRA6MiSA/p.jpeg?fv_content=true&size_mode=5',
+  role: ['Entry-Level DevOps Engineer'],
   // Refer https://ionicons.com/ and put the icon name inside iconName key
   connects: [
     {
-      name: 'Home',
-      iconName: 'logo-firefox',
-      link: 'https://sivanesh-s.github.io'
-    },
-    {
       name: 'Github',
       iconName: 'logo-github',
-      link: 'https://github.com/sivanesh-s'
+      link: 'https://github.com/dpodgit'
     },
     {
       name: 'Linkedin',
       iconName: 'logo-linkedin',
-      link: 'https://www.linkedin.com/in/sivanesh-shanmugam/'
+      link: 'https://www.linkedin.com/in/d-p-o-d/'
     },
     {
       name: 'StackOverflow',
       iconName: 'logo-stackoverflow',
-      link: 'https://stackoverflow.com/users/6940525/sivanesh-s'
-    },
-    {
-      name: 'Twitter',
-      iconName: 'logo-twitter',
-      link: 'https://twitter.com/sivanesh_fiz'
-    },
-    {
-      name: 'npm',
-      iconName: 'logo-npm',
-      link: 'https://www.npmjs.com/~sivanesh_s'
+      link: 'https://stackoverflow.com/users/10630709/david1592?tab=profile'
     },
     {
       name: 'Mail',
       iconName: 'mail-outline',
-      link: `mailto:${'yourmail@gmail.com'}?Subject=Hello%20again`
+      link: `mailto:${'davidpodwyer@gmail.com'}?Subject=Hello%20again`
     }
   ],
   links: [
-    { name: 'Projects', link: '#projects' },
-    { name: 'Blogs', link: '#blogs' }
+    { name: 'Blog', link: '/Users/davidodwyer/Documents/Projects/CV-Web-Mail-Server/portfolio-minimal/blog-entries.index.html' }
   ]
 };
