ec2fff029c3ff5325a0ea9ea46d7f10be05b822a
David O'Dwyer <public.dpod@gmail.com>
2021-01-12 20:16:35 +0000

    have added blog-entries section: using ssg5 and lowdown as static site generator, markdown to html; using and modifying terminalcss styling; editing for header

--- data.js
+++ data.js
@@ -1,19 +1,19 @@
 const main = {
   name: 'David O\'Dwyer',
   mail: 'davidpodwyer@gmail.com',
-  img: 'https://previews.dropbox.com/p/thumb/ABAJQZWzD3vvi9C7s7AwcU0LRnsLLbbbD_fSHbEdkRCpfpTJ1oR3dQOe2rah6_EVw1CDYg5EOGqoPmxW2Y05xBV04U-gmDzjaIkROVkzrLR_D3bQw0s1ZLHpzMRxaVxNVd8XLauTAMuJCXqCQ5V5E1zTWlVgMLhi7ILh9UdBi6TySxPE2Ig8XwlgUwOvS00GmY_UukGFDxXg7DNm4NPVQ8eAazL3xS1gPN-dja5ZH9zy9X-oUZGOuX2w_bnaHCCKxX400uM66hk886ihxH3c5_KJlVKYPviO6Ezqi4V2T6rk7oMsGghJdEisRz46mY4hBfHk03XCdlSVifVAePA5KOpC31OvX5RNfFZka6zRA6MiSA/p.jpeg?fv_content=true&size_mode=5',
+  img: 'images/photo.jpg',
   role: ['Entry-Level DevOps Engineer'],
   // Refer https://ionicons.com/ and put the icon name inside iconName key
   connects: [
     {
-      name: 'Github',
-      iconName: 'logo-github',
-      link: 'https://github.com/dpodgit'
-    },
-    {
       name: 'Linkedin',
       iconName: 'logo-linkedin',
       link: 'https://www.linkedin.com/in/d-p-o-d/'
+    },
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/dpodgit'
     },
     {
       name: 'StackOverflow',
@@ -24,9 +24,10 @@ const main = {
       name: 'Mail',
       iconName: 'mail-outline',
       link: `mailto:${'davidpodwyer@gmail.com'}?Subject=Hello%20again`
-    }
+    },
   ],
   links: [
-    { name: 'Blog', link: '/Users/davidodwyer/Documents/Projects/CV-Web-Mail-Server/portfolio-minimal/blog-entries.index.html' }
+    { name: 'About', link: 'blog-entries/dst/about.html' },
+    { name: 'Blog', link: 'blog-entries/dst/index.html' }
   ]
 };
